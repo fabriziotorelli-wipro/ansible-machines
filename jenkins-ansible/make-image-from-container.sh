637922152d65790f7d214267d111ef68a727cdff
Fabrizio Torelli <fabrizio.torelli@wipro.com>
2017-03-11 16:52:51 +0000

    Script correction

--- jenkins-ansible/make-image-from-container.sh
+++ jenkins-ansible/make-image-from-container.sh
@@ -3,10 +3,10 @@ if ! [[ -z "$(docker ps -a| grep jenkins-ansible)" ]]; then
    if ! [[ -z "$(docker ps| grep jenkins-ansible)" ]]; then
       docker stop jenkins-ansible
    fi
-   docker commit -a "Fabrizio Torelli <fabrizio.torelli@wipro.com>" -m "Microservices Pipeline - Ansible Jenkins" -c "ENV PLAYBOOKS=microservices-recreate" -c "CMD rm -f /usr/local/share/ansible/playbook/.installed"  jenkins-ansible builditftorelli/jenkins-microservices:2.32.3
+   docker commit -a "Fabrizio Torelli <fabrizio.torelli@wipro.com>" -m "Microservices Pipeline - Ansible Jenkins" -c "ENV PLAYBOOKS=microservices-recreate" -c "CMD rm -f /usr/local/share/ansible/playbook/.installed && /usr/local/share/ansible/playbook/run_ansible_playbook.sh"  jenkins-ansible builditftorelli/jenkins-microservices:2.32.3
    echo "Starting a test container ..."
-   docker run -d --name jenkins-microservice -p 8080:8080 -p 50000:50000 -it builditftorelli/jenkins-microservices:2.32.3
-   docker logs -f jenkins-microservice
+   docker run -d --name jenkins-microservices -p 8080:8080 -p 50000:50000 -it builditftorelli/jenkins-microservices:2.32.3
+   docker logs -f jenkins-microservices
 else
    echo "No image found. Please run test.sh before. Create the default microservices image and then you can pack-it in an image!!"
-if
+fi
